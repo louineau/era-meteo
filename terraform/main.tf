bdbae3e1ffda3078abb4858e05ed0870efdc5ebf
Noah Louineau <38557736+louineau@users.noreply.github.com>
2025-04-24 15:20:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ variable "ssh_public_key" {
 }
 
 variable "ssh_private_key" {
-  description = "The path to the SSH private key for connecting to the EC2 instance"
+  description = "The SSH private key for connecting to the EC2 instance"
   type        = string
   sensitive   = true
 }
@@ -18,6 +18,26 @@ resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key-${timestamp()}"
   public_key = var.ssh_public_key
 }
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0e449927258d45bc4"  # Remplacez par l'AMI appropriée pour votre région
@@ -49,8 +69,11 @@ resource "aws_instance" "app_server" {
       user        = "ec2-user"
       private_key = var.ssh_private_key # Remplacez par le chemin vers votre clé privée
       host        = self.public_ip
+      timeout     = "10m" 
     }
   }
+  
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
 }
 
 output "instance_public_ip" {
