74c0bf2d9f7282d04a1aa690afc2b11f82f839e7
Noah Louineau <38557736+louineau@users.noreply.github.com>
2025-04-24 11:59:12 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-west-2" # Vous pouvez choisir une autre région si vous préférez
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # AMI pour Ubuntu 20.04 LTS (éligible pour le niveau gratuit)
-  instance_type = "t2.micro"              # Type d'instance éligible pour le niveau gratuit
+  ami           = "ami-0e449927258d45bc4"  # Remplacez par l'AMI appropriée pour votre région
+  instance_type = "t2.micro"
 
   tags = {
     Name = "EraMeteoServer"
@@ -21,10 +12,33 @@ resource "aws_instance" "app_server" {
 
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y docker.io
-              docker run -d -p 80:3000 teralti/era-meteo
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo docker run -d -p 80:3000 teralti/era-meteo:latest
               EOF
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker pull teralti/era-meteo:latest",
+      "sudo docker stop era-meteo-container || true",
+      "sudo docker rm era-meteo-container || true",
+      "sudo docker run -d -p 80:3000 --name era-meteo-container teralti/era-meteo:latest"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = filebase64("${var.ssh_key}")
+      host        = self.public_ip
+    }
+  }
+}
+
+variable "ssh_key" {
+  description = "The SSH private key for connecting to the EC2 instance"
+  type        = string
+  sensitive   = true
 }
 
 output "instance_public_ip" {
