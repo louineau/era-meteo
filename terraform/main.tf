bd91d96f6ed755551212b4dc5de2c628a25356c0
Noah Louineau <38557736+louineau@users.noreply.github.com>
2025-04-24 15:36:27 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,7 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # à restreindre en prod
   }
 
   egress {
