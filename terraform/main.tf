ca3c180ffdaf363539c807ba0693502a56fe6f2b
Noah Louineau <38557736+louineau@users.noreply.github.com>
2025-04-24 13:57:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "ssh_public_key" {
+  description = "The SSH public key for connecting to the EC2 instance"
+  type        = string
+  sensitive   = true
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = var.ssh_public_key
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0e449927258d45bc4"  # Remplacez par l'AMI appropriée pour votre région
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "EraMeteoServer"
@@ -29,7 +41,7 @@ resource "aws_instance" "app_server" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = filebase64("${var.ssh_key}")
+      private_key = file("${path.module}/private_key")  # Remplacez par le chemin vers votre clé privée
       host        = self.public_ip
     }
   }
