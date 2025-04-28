bd918be098285a7f1fb27a1d059ec395a5e98ac4
Noah Louineau <38557736+louineau@users.noreply.github.com>
2025-04-28 12:10:57 +0200

    Update variables.tf

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -1,9 +1,1 @@
-variable "ssh_private_key" {
-  description = "Clé SSH privée pour accéder à l'instance EC2 existante"
-  type        = string
-}
 
-variable "instance_public_ip" {
-  description = "IP publique de l'instance EC2 existante"
-  type        = string
-}
